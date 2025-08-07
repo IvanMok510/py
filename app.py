0b28e3a3fb5b99153b0184cfccaf056d14e4a553
IvanMok510 <ivanmok510@gmail.com>
2025-08-07 15:43:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World! I am running on Python in OpenShiftwwwwwwwwwwww!\n'
+    return 'Hello World! I am running on Python in OpenShift!\nThis is a demo\n'
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000)) 
