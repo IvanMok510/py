294b1739452e05f099cdb947ce84168e71db6d1f
IvanMok510 <ivanmok510@gmail.com>
2025-08-13 11:11:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ def hello_world():
     return 'Hello World! I am running on Python in OpenShift!\n'
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000)) 
+    port = int(os.environ.get('PORT', 8080)) 
     app.run(host='0.0.0.0', port=port)
