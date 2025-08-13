f0c8863081973ccd199b18719286b333add6402c
IvanMok510 <ivanmok510@gmail.com>
2025-08-13 12:02:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 from flask import Flask
-import os  # Add this import for dynamic port
-
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World! I am running on Python in OpenShift!\n'
+def hello():
+    return "Hello from Python on OpenShift!"
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080)) 
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=8080)
